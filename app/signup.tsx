d9d9538263553c3a7647c98e2bb9e69bd575f376
KaberaSamuel <kaberanshutis@gmail.com>
2025-08-29 21:11:31 +0200

    added logic for registering user

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,13 +1,52 @@
 import { Colors } from "@/constants/globalStyles";
+import axios from "axios";
 import { Link } from "expo-router";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+
+interface SignupFormTypes {
+  email: string;
+}
 
 export default function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { control, handleSubmit } = useForm<SignupFormTypes>();
+  const baseApi = "https://c45e4c8ccb8d.ngrok-free.app/users";
+
+  const onSubmit = async (data: SignupFormTypes) => {
+    try {
+      setIsLoading(true);
+      await axios.post(`${baseApi}/register/`, data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Email or Phone number</Text>
-      <TextInput placeholder="example@gmail.com" />
-      <Text style={styles.next}>Next</Text>
+      <Text style={styles.label}>Email </Text>
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="example@gmail.com"
+          />
+        )}
+        name="email"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+
+      <Pressable onPress={handleSubmit(onSubmit)}>
+        <Text style={styles.next}>Next</Text>
+      </Pressable>
       <Link href="/login" style={styles.login}>
         Login
       </Link>
